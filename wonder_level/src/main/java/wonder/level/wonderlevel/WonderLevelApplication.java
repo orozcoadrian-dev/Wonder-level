f30be7bbe74c4bc9e548ca8645a046a10acba7f8
orozcoadrian-dev <adrianenriqueorozco@gmail.com>
2026-09-21 23:42:17 -0500

    docs: documentación y creación de archivos importantes.

---
+++ wonder_level/src/main/java/wonder/level/wonderlevel/WonderLevelApplication.java
@@ -0,0 +1,11 @@
+package wonder.level.wonderlevel;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WonderLevelApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WonderLevelApplication.class, args);
+    }
+}
